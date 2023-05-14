2ff2bcdec61117df863e4423153e75d5df13f0de
Diwakar Varanasi <vinaydiwakar10@gmail.com>
2023-05-14 10:37:11 +0530

    Lexer is completed. Number,string,comnt,identifier

--- teenytiny.py
+++ teenytiny.py
@@ -2,12 +2,13 @@ from lex import *
 
 
 def main():
-    source="LET age = 25"
+    source = "IF+-123 foo*THEN/"
     lexer = Lexer(source)
 
-    while lexer.peek() != '\0':
-        print(lexer.curChar)
-        lexer.nextChar()
+    token = lexer.getToken()
+    while token.kind != TokenType.EOF:
+        print(token.kind)
+        token = lexer.getToken()
 
 if __name__ == "__main__":
     main()
